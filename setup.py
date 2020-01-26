bbbd45dcdbb5c4fd1c64141f9b6e6fb547158401
Danny Staple <danny@orionrobots.co.uk>
2020-01-26 19:24:47 +0000

    Don't actually need six yet.

--- setup.py
+++ setup.py
@@ -13,7 +13,6 @@ setuptools.setup(
     long_description_content_type="text/markdown",
     url="https://github.com/orionrobots/Raspi_MotorHAT",
     packages=setuptools.find_packages(),
-    install_requires=("six"),
     classifiers=(
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
